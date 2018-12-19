0080d53c9e65911a0f65795f9bcf04afbb16e109
Mateusz Łaś <mateuszlas@hotmail.com>
2018-12-19 13:13:20 +0100

    add

---
+++ Warehouse_Rys/Model/DateSqlite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Rys
+{
+    public class DateSqlite : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
