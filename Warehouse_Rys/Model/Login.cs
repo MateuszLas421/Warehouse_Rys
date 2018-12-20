1982e03bfc19114ea4f67edfd0b244045cad835e
Mateusz Łaś <mateuszlas@hotmail.com>
2018-12-20 19:50:42 +0100

    add class and modify

---
+++ Warehouse_Rys/Model/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Warehouse_Rys.Model
+{
+    public class Login : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        private string _login, _password;
+        private Boolean ok;
+
+        public Login()
+        {
+            Ok = false;
+        }
+
+        public string Login1 { get => _login;
+            set
+            {
+                if (_login != value) {_login = value; OnPropertyChanged("Login1");}
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value) { _password = value; OnPropertyChanged("Password"); }
+            }
+        }
+        public Boolean Ok
+        {
+            get => ok;
+            set
+            {
+                if (ok != value) { ok = value; OnPropertyChanged("Ok"); }
+            }
+        }
+    }
+}
---
+++ Warehouse_Rys/ViewModel/SqliteLoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Warehouse_Rys.ViewModel
+{
+    class SqliteLoginVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+        private SQLiteDataAdapter m_oDataAdapter = nuul;
+    }
+}
