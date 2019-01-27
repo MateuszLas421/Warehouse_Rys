8ce79848c14b9264c0224c5949c07f2fe103c882
Mateusz Łaś <mateuszlas@hotmail.com>
2019-01-27 09:58:23 +0100

    abstract

--- Warehouse_Rys/Produkt.cs
+++ Warehouse_Rys/Produkt.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Warehouse_Rys
 {
-    class Produkt
+    abstract class Produkt
     {
         public int ID { get; set; }
         public string Name { get; set; }
